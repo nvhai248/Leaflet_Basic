42531bc0ed68e8f0e257d1fcd8186d4479d528fb
20120468 <20120468@student.hcmus.edu.vn>
2023-05-12 22:52:20 +0700

    first commit

---
+++ data/point.js
@@ -0,0 +1,60 @@
+var pointJson = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "coordinates": [
+                    106.77539015290262,
+                    10.88481781792754
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "coordinates": [
+                    106.78082348528193,
+                    10.885612477737325
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "coordinates": [
+                    106.77854860654031,
+                    10.871291646050011
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "coordinates": [
+                    106.79586909115847,
+                    10.871843484996234
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "coordinates": [
+                    106.79951871087474,
+                    10.891777292893892
+                ],
+                "type": "Point"
+            }
+        }
+    ]
+}
